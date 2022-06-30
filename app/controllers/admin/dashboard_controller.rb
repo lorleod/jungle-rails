dc153c23b205c79c57ce4db9a3fa55ccbbf493a3
lorleod <taylor.mcleod@gmail.com>
2022-06-30 03:45:29 +0000

    added dashboard functionality

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
+    @products = Product.all.order(created_at: :desc)
+    @categories = Category.all.order(created_at: :desc)
   end
 end
